e7b3e7ca76d361ba5282bb0add0099aa824761ea
Brandon Gathright <btgathright@tamu.edu>
2020-02-29 13:24:14 -0600

    Initialization
    Initialized files with setup and run scripts.
    After cloning, run "./setup.txt" in Git Bash or Unix terminal.
    Run the program by right clicking on "jdbcpostgreSQLGUI.java" file and selecting "run"

---
+++ src/dbSetup.java
@@ -0,0 +1,10 @@
+/*
+Robert Lightfoot
+CSCE 315
+Database setup example
+Purpose: Keeping usernames and password private.
+*/
+public final class dbSetup  {
+  public static final String user = "btgathright";
+  public static final String pswd = "526000548";
+}//end class
